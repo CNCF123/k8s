27f78e60ae6fd64084d6a42bad29be9975fb06fd
向上的信念 <19582627+xsdxn@users.noreply.github.com>
2020-06-21 20:05:19 +0800

    Update install-node.sh

--- install/install-node.sh
+++ install/install-node.sh
@@ -56,7 +56,8 @@ cat > /etc/docker/daemon.json <<EOF
     "devops-hub.tutorabc.com.cn"
 ],
 "registry-mirror": "https://5cs233bb.mirror.aliyuncs.com",
-"graph": "/data/docker"
+"graph": "/data/docker",
+"exec-opts": ["native.cgroupdriver=systemd"]
 }
 EOF
 
