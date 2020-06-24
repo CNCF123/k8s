80437bc8be765e4cf4d9b37dc7f8bf1126addde4
向上的信念 <19582627+xsdxn@users.noreply.github.com>
2020-06-24 09:49:35 +0800

    Update install-master.sh

--- install/install-master.sh
+++ install/install-master.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #安装master节点
 
-DOCKER-VERSION="19.03.9"
-K8S-VERSION="v1.14.3"
+DockerVersion="19.03.9"
+K8sVersion="v1.14.3"
 
 #关闭SELinux
 sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
@@ -51,7 +51,7 @@ yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/
 # 安装指定版本的Docker-CE:
 # yum list docker-ce.x86_64 --showduplicates | sort -r
 #安装docker
-yum install -y docker-ce-${DOCKER-VERSION} docker-ce-cli-${DOCKER-VERSION} containerd.io
+yum install -y docker-ce-${DockerVersion} docker-ce-cli-${DockerVersion} containerd.io
 
 #创建docker配置文件
 mkdir -p /etc/docker
@@ -93,7 +93,7 @@ EOF
 ### yum list kubelet kubeadm --showduplicates|sort -r
 
 #安装指定的软件包 kubelet-1.14.3 kubeadm-1.14.3 kubectl-1.14.3
-yum install -y kubelet-${K8S-VERSION} kubectl-${K8S-VERSION} kubeadm-${K8S-VERSION}
+yum install -y kubelet-${K8sVersion} kubectl-${K8sVersion} kubeadm-${K8sVersion} --setopt=obsoletes=0
 #设置开机自动启动kubelet
 systemctl enable kubelet.service
 
